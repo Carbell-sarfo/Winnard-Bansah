a5b572f5f2e65080405c5cd319bab7ba07f98ad0
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2024-05-23 11:30:03 +0000

    gsap animations added

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -1,10 +1,36 @@
 import { gsap } from "gsap";
 
-gsap.to(".box", {
-    x: '80vw',
-    repeat: -1,
-    yoyo: true,
-    duration: 2,
-    ease: 'power3.inOut'
 
+var tl = gsap.timeline()
+
+
+// hero image
+tl.to(".hero-img",  {
+    opacity: 1,
+    y: 0,
+    duration: 2,
+    scale: 1,
+} )
+
+
+
+// Second Timeline for Hero Text
+var secondTl = gsap.timeline()
+
+secondTl.fromTo(".hero-h1", {x: 100, opacity: 0}, {
+    opacity: 1,
+    x: 0,
+})
+
+// hero text
+secondTl.fromTo(".hero-text", {x: 100, opacity: 0}, {
+    opacity: 1,
+    x: 0,
+})
+
+
+// hero buttons
+secondTl.fromTo(".hero-btn", {opacity: 0,}, {
+    opacity: 1,
+    y: 0,
 })
